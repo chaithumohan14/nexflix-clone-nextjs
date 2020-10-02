55a4f56d20f21d3317972e2fa06f381d2923d6d2
chaithumohan <51069171+chaithumohan@users.noreply.github.com>
2020-10-02 14:11:08 +0530

    1

--- components/LoggedIn.js
+++ components/LoggedIn.js
@@ -1,52 +1,9 @@
 import Navbar from "./Navbar/Navbar";
 import styles from "../styles/loggedin.module.css";
 import Page from "./Page";
-import { useState, useEffect } from "react";
 import CardRow from "./CardRow/CardRow";
-function LoggedIn() {
-  //
-  const API_KEY = "4f2d540fb28b05b52c0443b24ff06b73";
-  const URL = "http://api.themoviedb.org/3";
-  const suffixes = {
-    trending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
-    originals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
-    topRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US,`,
-    actionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
-    comedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
-    horrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
-    romanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
-    documenatary: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
-  };
-
-  const [originals, setOriginals] = useState([]);
-  const [trending, setTrending] = useState([]);
-  const [topRated, setTopRated] = useState([]);
-  const [action, setAction] = useState([]);
-  const [comedy, setComedy] = useState([]);
-  const [horror, setHorror] = useState([]);
-  const [romance, setRomance] = useState([]);
-  const [documentary, setDocumentary] = useState([]);
-
-  useEffect(() => {
-    const getData = async (url, func) => {
-      const resp = null;
-      await fetch(`${URL}${url}`)
-        .then((res) => res.json())
-        .then((data) => func(data.results))
-        .catch((err) => console.log(err.message));
-      console.log("resp", resp);
-      return resp;
-    };
-    console.log();
-    getData(suffixes.originals, setOriginals);
-    getData(suffixes.trending, setTrending);
-    getData(suffixes.topRated, setTopRated);
-    getData(suffixes.actionMovies, setAction);
-    getData(suffixes.comedyMovies, setComedy);
-    getData(suffixes.horrorMovies, setHorror);
-    getData(suffixes.romanceMovies, setRomance);
-    getData(suffixes.documenatary, setDocumentary);
-  }, []);
+function LoggedIn({ data }) {
+  console.log(data, "from logged in");
 
   return (
     <Page>
@@ -63,14 +20,18 @@ function LoggedIn() {
           murderous...
         </div>
       </div>
-      <CardRow key={"Originals"} title={"Originals"} datas={originals} />
-      <CardRow key={"Trending"} title={"Trending"} datas={trending} />
-      <CardRow key={"topRated"} title={"top Rated"} datas={topRated} />
-      <CardRow key={"action"} title={"action"} datas={action} />
-      <CardRow key={"comedy"} title={"comedy"} datas={comedy} />
-      <CardRow key={"horror"} title={"horror"} datas={horror} />
-      <CardRow key={"romance"} title={"romance"} datas={romance} />
-      <CardRow key={"documentary"} title={"documentary"} datas={documentary} />
+      <CardRow key={"Originals"} title={"Originals"} datas={data.trending} />
+      <CardRow key={"Trending"} title={"Trending"} datas={data.originals} />
+      <CardRow key={"topRated"} title={"top Rated"} datas={data.topRated} />
+      <CardRow key={"action"} title={"action"} datas={data.actionMovies} />
+      <CardRow key={"comedy"} title={"comedy"} datas={data.comedyMovies} />
+      <CardRow key={"horror"} title={"horror"} datas={data.horrorMovies} />
+      <CardRow key={"romance"} title={"romance"} datas={data.romanceMovies} />
+      <CardRow
+        key={"documentary"}
+        title={"documentary"}
+        datas={data.documenatary}
+      />
     </Page>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -6,19 +6,65 @@ import Loader from "./../components/Loader";
 import { useState, useEffect } from "react";
 import { auth } from "../_firebase";
 
-function index() {
+function index({ data }) {
   const [store] = Store();
   const [Component, setComponent] = useState(<Loader />);
   useEffect(() => {
     auth.onAuthStateChanged((authUser) => {
       if (auth.currentUser) {
-        setComponent(<LoggedIn />);
+        setComponent(<LoggedIn data={data} />);
       } else {
         setComponent(<NoLogin />);
       }
     });
   }, []);
   return <Page>{Component}</Page>;
+}
+
+export async function getServerSideProps() {
+  console.log("Server Side");
+  const getData = async (url) => {
+    let resp = null;
+    await fetch(`${URL}${url}`)
+      .then((res) => res.json())
+      .then((data) => {
+        resp = data.results;
+      })
+      .catch((err) => console.log(err.message));
+    return resp;
+  };
+  const API_KEY = "4f2d540fb28b05b52c0443b24ff06b73";
+  const URL = "http://api.themoviedb.org/3";
+  const suffixes = {
+    trending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    originals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    topRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US,`,
+    actionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    comedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    horrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    romanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    documenatary: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+  };
+  let trending = await getData(suffixes.trending);
+  let originals = await getData(suffixes.originals);
+  let topRated = await getData(suffixes.topRated);
+  let actionMovies = await getData(suffixes.actionMovies);
+  let comedyMovies = await getData(suffixes.comedyMovies);
+  let horrorMovies = await getData(suffixes.horrorMovies);
+  let romanceMovies = await getData(suffixes.romanceMovies);
+  let documenatary = await getData(suffixes.documenatary);
+
+  const props = {
+    trending: trending,
+    originals: originals,
+    topRated: topRated,
+    actionMovies: actionMovies,
+    comedyMovies: comedyMovies,
+    horrorMovies: horrorMovies,
+    romanceMovies: romanceMovies,
+    documenatary: documenatary,
+  };
+  return { props: { data: props } };
 }
 
 export default index;
